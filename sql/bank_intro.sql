4f9c7cf9b70ef09a7028abc0c9a0ca8d598cbc5f
Tymon Nosewicz <nosewicz.tymon@gmail.com>
2026-03-13 08:05:22 +0100

    blblblblblblbl

--- sql/bank_intro.sql
+++ sql/bank_intro.sql
@@ -1,17 +1,19 @@
-# 
-
-# to jest komentarz - wszystko w linijce po znaku # jest ignorowane podczas wykonania kodu
-
-# nasze pierwsze zapytanie - zwraca wszystkie dane z tabeli bank_client
-
-select * from bank_client; # średnik oznacza koniec zapytania - gdyby go nie było wszystkie linijki poniżej byłyby traktowane jako jego dalszy ciąg
-
-# 1. PROJEKCJA - wybranie wyłącznie określonych kolumn z tabeli
 
 
+-- to jest komentarz - wszystko w linijce po znaku # jest ignorowane podczas wykonania kodu
 
-# 2. SELEKCJA - wybranie z tabeli wierszy spełniających dany warunek
+-- nasze pierwsze zapytanie - zwraca wszystkie dane z tabeli bank_client
+
+select * from bank_client; 
+
+--  średnik oznacza koniec zapytania - gdyby go nie było wszystkie linijki poniżej byłyby traktowane jako jego dalszy ciąg
+
+-- 1. PROJEKCJA - wybranie wyłącznnoseie określonych kolumn z tabeli
 
 
 
-# 3. SORTOWANIE
+-- 2. SELEKCJA - wybranie z tabeli wierszy spełniających dany warunek
+
+
+
+-- 3. SORTOWANIE  
