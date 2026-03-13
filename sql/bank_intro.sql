1b0bacd0c1974c594dec674779d7b3f521b54d2d
Jan Grabowski <grab-ja2@nurt.net.pl>
2026-03-13 08:11:26 +0100

    Zmiana # na --

--- sql/bank_intro.sql
+++ sql/bank_intro.sql
@@ -1,17 +1,17 @@
-# 
+--
 
-# to jest komentarz - wszystko w linijce po znaku # jest ignorowane podczas wykonania kodu
+-- to jest komentarz - wszystko w linijce po znaku # jest ignorowane podczas wykonania kodu
 
-# nasze pierwsze zapytanie - zwraca wszystkie dane z tabeli bank_client
+--nasze pierwsze zapytanie - zwraca wszystkie dane z tabeli bank_client
 
 select * from bank_client; # średnik oznacza koniec zapytania - gdyby go nie było wszystkie linijki poniżej byłyby traktowane jako jego dalszy ciąg
 
-# 1. PROJEKCJA - wybranie wyłącznie określonych kolumn z tabeli
+--1. PROJEKCJA - wybranie wyłącznie określonych kolumn z tabeli
 
 
 
-# 2. SELEKCJA - wybranie z tabeli wierszy spełniających dany warunek
+--2. SELEKCJA - wybranie z tabeli wierszy spełniających dany warunek
 
 
 
-# 3. SORTOWANIE
+--3. SORTOWANIE
