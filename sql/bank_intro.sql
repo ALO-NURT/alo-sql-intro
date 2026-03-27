3e379de54e3e5ff69be5114b904ccdab60424963
Michal Swiezy <michal.swiezy@nurt.edu.pl>
2026-03-27 08:15:22 +0100

    Lekcja 27.03

--- sql/bank_intro.sql
+++ sql/bank_intro.sql
@@ -1,3 +1,4 @@
+-- Active: 1774593734735@@alo-nurt.pl@3306@alo_test_db
 -- 
 
 -- to jest komentarz - wszystko w linijce po znaku # jest ignorowane podczas wykonania kodu
@@ -8,10 +9,11 @@ select * from bank_client; -- średnik oznacza koniec zapytania - gdyby go nie b
 
 -- 1. PROJEKCJA - wybranie wyłącznie określonych kolumn z tabeli
 
-
+select pesel, first_name, last_name, phone from bank_client;
 
 -- 2. SELEKCJA - wybranie z tabeli wierszy spełniających dany warunek
 
-
+select * from bank_client 
+where first_name = "Anna";
 
 -- 3. SORTOWANIE
