26f16ea816c8d1e57520e9347df72fedbd10ab1d
Laura Dubarek <Duba-La@nurt.net.pl>
2026-03-27 08:16:33 +0100

    LD

--- sql/bank_intro.sql
+++ sql/bank_intro.sql
@@ -1,15 +1,15 @@
+-- Active: 1774594058648@@alo-nurt.pl@3306@alo_test_db
 # qa64
 
 -- to jest komentarz - wszystko w linijce po znaku # jest ignorowane podczas wykonania kodu
 
 -- nasze pierwsze zapytanie - zwraca wszystkie dane z tabeli bank_client
 
-select * from bank_client; # średnik oznacza koniec zapytania - gdyby go nie było wszystkie linijki poniżej byłyby traktowane jako jego dalszy ciąg
 
 -- 1. PROJEKCJA - wybranie wyłącznie określonych kolumn z tabeli
 
 
-
+SELECT pesel, first_name, last_name FROM bank_client
 -- 2. SELEKCJA - wybranie z tabeli wierszy spełniających dany warunek
 
 
