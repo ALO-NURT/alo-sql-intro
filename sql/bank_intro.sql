e02a385c7b1866cb012beeb4d75860e0738253fb
Mikołaj Musiał <musi-mi@nurt.net.pl>
2026-04-17 08:02:50 +0200

    Zmiana komentarzy

--- sql/bank_intro.sql
+++ sql/bank_intro.sql
@@ -1,17 +1,18 @@
-# 
+-- Active: 1776404050727@@alo-nurt.pl@3306@alo_test_db
+-- 
 
-# to jest komentarz - wszystko w linijce po znaku # jest ignorowane podczas wykonania kodu
+-- to jest komentarz - wszystko w linijce po znaku -- jest ignorowane podczas wykonania kodu
 
-# nasze pierwsze zapytanie - zwraca wszystkie dane z tabeli bank_client
+-- nasze pierwsze zapytanie - zwraca wszystkie dane z tabeli bank_client
 
-select * from bank_client; # średnik oznacza koniec zapytania - gdyby go nie było wszystkie linijki poniżej byłyby traktowane jako jego dalszy ciąg
+select * from bank_client; -- średnik oznacza koniec zapytania - gdyby go nie było wszystkie linijki poniżej byłyby traktowane jako jego dalszy ciąg
 
-# 1. PROJEKCJA - wybranie wyłącznie określonych kolumn z tabeli
-
-
-
-# 2. SELEKCJA - wybranie z tabeli wierszy spełniających dany warunek
+-- 1. PROJEKCJA - wybranie wyłącznie określonych kolumn z tabeli
 
 
 
-# 3. SORTOWANIE
+-- 2. SELEKCJA - wybranie z tabeli wierszy spełniających dany warunek
+
+
+
+-- 3. SORTOWANIE
