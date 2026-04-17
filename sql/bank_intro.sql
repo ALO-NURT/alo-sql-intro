20b0f71be963299bfa32961943026a7d4ae9a896
Franek Jot <jask-fr@nurt.net.pl>
2026-04-17 07:59:34 +0200

    commit_sortowanie

--- sql/bank_intro.sql
+++ sql/bank_intro.sql
@@ -1,17 +1,21 @@
+-- Active: 1774593739771@@alo-nurt.pl@3306@alo_test_db
 -- 
 
 -- to jest komentarz - wszystko w linijce po znaku # jest ignorowane podczas wykonania kodu
 
 -- nasze pierwsze zapytanie - zwraca wszystkie dane z tabeli bank_client
 
-select * from bank_client; -- średnik oznacza koniec zapytania - gdyby go nie było wszystkie linijki poniżej byłyby traktowane jako jego dalszy ciąg
+SELECT * FROM bank_client; -- średnik oznacza koniec zapytania - gdyby go nie było wszystkie linijki poniżej byłyby traktowane jako jego dalszy ciąg
 
 -- 1. PROJEKCJA - wybranie wyłącznie określonych kolumn z tabeli
 
-
+select * from bank_client
+where first_name = "Anna";
 
 -- 2. SELEKCJA - wybranie z tabeli wierszy spełniających dany warunek
 
 
 
 -- 3. SORTOWANIE
+select*from bank_client
+order by pesel;
