4b1143f47fb48e4841da7ca881029ed45384dbd6
Nicolas164-png <mikolaj@musial.eu.org>
2026-05-07 19:55:09 +0200

    OK

--- sql/bank_intro.sql
+++ sql/bank_intro.sql
@@ -10,6 +10,9 @@ describe bank_account;
 describe bank_transaction;
 
 # 1. PROJEKCJA - wybranie wyłącznie określonych kolumn z tabeli
+
+#SELECT <PROJEKCJA> FROM TABLE;
+
 #wszystkie rekordy i kolumny
 select * from bank_client; 
 #wybrane kolumny
@@ -17,13 +20,32 @@ select id, first_name, last_name, city from bank_client;
 #wybrane kolumny inna kolejnosc
 select first_name, last_name, email from bank_client; 
 # 2. SELEKCJA - wybranie z tabeli wierszy spełniających dany warunek
-
+#SELECT <PROJEKCJA> FROM TABLE WHERE <SELEKCJA>;
 #wszystkie z imieniem na A
 select first_name, last_name, email from bank_client WHERE first_name LIKE 'A%'; 
+select * from bank_client WHERE first_name LIKE 'A%'; 
+select * from bank_client WHERE city = 'Wrocław' AND postal_code LIKE '50%'; 
 
+select * from bank_client WHERE id>2 and id<10 and id<>5 or id=20;
+select first_name from bank_client WHERE first_name LIKE 'E%' AND first_name LIKE '%na' or first_name='ANNA';
 
 # 3. SORTOWANIE
+#SELECT <PROJEKCJA> FROM TABLE WHERE <SELEKCJA> ORDER BY <SORTOWANIE>;
 
+#sortowanie proste
+select * from bank_client ORDER BY city; 
+#sortowanie proste odwrotna kole4jnoscv
+select * from bank_client ORDER BY city DESC; 
+#sortowanie po wielu komumnach
+select * from bank_client ORDER BY city ASC, last_name DESC; 
+select * from bank_client ORDER BY city ASC, last_name DESC, id; 
+
+##### LACZENIE TABEL
+
+select * from bank_client INNER JOIN bank_account ON bank_client.id = bank_account.client_id;
+select * from bank_client INNER JOIN bank_account ON bank_client.id = bank_account.client_id INNER JOIN bank_transaction ON bank_account.id=bank_transaction.account_id;
+
+select * from bank_transaction;
 
 
 
