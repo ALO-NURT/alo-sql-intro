660743f2aa025ad2c79809321297638c0d30895e
Nicolas164-png <mikolaj@musial.eu.org>
2026-05-07 19:03:43 +0200

    Zmiany MM

--- sql/bank_intro.sql
+++ sql/bank_intro.sql
@@ -1,10 +1,16 @@
+-- Active: 1778173118744@@alo-nurt.pl@3306@alo_test_db
 # 
 
 # to jest komentarz - wszystko w linijce po znaku # jest ignorowane podczas wykonania kodu
 
 # nasze pierwsze zapytanie - zwraca wszystkie dane z tabeli bank_client
 
-select * from bank_client; # średnik oznacza koniec zapytania - gdyby go nie było wszystkie linijki poniżej byłyby traktowane jako jego dalszy ciąg
+describe bank_client;
+describe bank_account;
+
+describe bank_transaction;
+
+select * from bank_client; 
 
 # 1. PROJEKCJA - wybranie wyłącznie określonych kolumn z tabeli
 
