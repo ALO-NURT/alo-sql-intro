bdc52c8cf39bbd35c56d09e0cca5a2670a07ef25
Nicolas164-png <mikolaj@musial.eu.org>
2026-05-07 19:34:38 +0200

    ok

--- sql/bank_intro.sql
+++ sql/bank_intro.sql
@@ -7,17 +7,38 @@
 
 describe bank_client;
 describe bank_account;
-
 describe bank_transaction;
 
-select * from bank_client; 
-
 # 1. PROJEKCJA - wybranie wyłącznie określonych kolumn z tabeli
-
-
-
+#wszystkie rekordy i kolumny
+select * from bank_client; 
+#wybrane kolumny
+select id, first_name, last_name, city from bank_client; 
+#wybrane kolumny inna kolejnosc
+select first_name, last_name, email from bank_client; 
 # 2. SELEKCJA - wybranie z tabeli wierszy spełniających dany warunek
 
+#wszystkie z imieniem na A
+select first_name, last_name, email from bank_client WHERE first_name LIKE 'A%'; 
 
 
 # 3. SORTOWANIE
+
+
+
+
+# CWICZENIA DONOWE
+
+#CREATE TABLE tabelka ( 
+#    id INT,
+ #   Name VARCHAR(50),
+ #   czas timestamp
+#);
+#INSERT INTO tabelka VALUES (1, 'Jakis tekst', '2026-01-01 13:00:00');
+#INSERT INTO tabelka VALUES (2, 'Jakis tekst 2', '2026-01-02 13:00:00');
+#INSERT INTO tabelka VALUES (3, 'Jakis tekst 3', '2026-01-03 13:00:00');
+#INSERT INTO tabelka VALUES (4, 'Jakis tam 4', '2026-01-03 13:00:00');
+#INSERT INTO tabelka VALUES (5, 'Jakis tam 5', '2026-01-03 13:00:00');
+#INSERT INTO tabelka VALUES (6, 'Jakis tam 3', '2026-01-03 13:00:00');
+
+#select * from tabelka WHERE name LIKE '%3%' AND id>=3;
